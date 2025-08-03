52a205bc82508acd593ff7df48a4e3d0d9b9d616
Jhojan Mendoza <Jhojanbautista11@gmail.com>
2025-08-03 03:25:52 -0500

    Primer commit del proyecto Yenni

---
+++ 1/script.js
@@ -0,0 +1,27 @@
+    class FlipBook{
+        constructor(bookElem){
+            this.book = bookElem;
+            this.leaves = [...bookElem.querySelectorAll('.hoja')];
+            this.current = 0;
+            this.max = this.leaves.length;
+            this.init();
+        }
+        init(){
+            this.update();
+            this.book.addEventListener('click', e => {
+                const {left, width} = this.book.getBoundingClientRect();
+                const x = e.clientX - left;
+                if(x > width/2 && this.current < this.max) this.current++;
+                if(x <= width/2 && this.current > 0) this.current--;
+                this.update();
+            });
+        }
+        update(){
+            this.leaves.forEach((leaf, idx) => {
+                const pos = idx < this.current;
+                leaf.style.transform = pos ? 'rotateY(-180deg)' : 'rotateY(0deg)';
+                leaf.style.zIndex = pos ? idx : this.max - idx;
+            });
+        }
+    }
+    document.addEventListener('DOMContentLoaded', ()=> new FlipBook(document.getElementById('flipbook')));
